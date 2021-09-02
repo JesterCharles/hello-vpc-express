0e0e2a1ca2945cf64d47a8637462a9d42156e36d
Charles Jester <jestercharles@gmail.com>
2021-09-02 13:23:07 -0400

    made test vpc express site

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const cors = require("cors")
+const app = express()
+app.use(cors())
+
+let counter =0;
+const id = Math.random();
+
+app.get("/hello", (req,res)=>{
+    res.send(`Hello ID number ${id} you are visitor number: ` + ++counter)
+})
+
+app.listen(3000, ()=>{console.log("Application is up and running! :D")})
